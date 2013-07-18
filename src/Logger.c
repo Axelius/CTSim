f946eaff76f796dd4c860ac9b88518b5758d4d18
Alexander Winkler <alexander.winkler@mytum.de>
2013-07-18 19:00:07 +0200

    Logger can now also log to file

--- src/Logger.c
+++ src/Logger.c
@@ -12,7 +12,7 @@ void _logItVerb(int messageLevel, const char * file, const char * function, cons
 	if(messageLevel <= LOGLEVEL){
 		va_list args;
 		va_start(args, fmt);
-		char msg[256];
+		char msg[MAXMESSAGELENGTH];
 		switch(messageLevel){
 #if LOGLEVEL >= TRACE
 		case TRACE:	printf("[[LogIt-TRACE][%s].%s.%d] ", file, function, line); break;
@@ -53,7 +53,7 @@ void _logItNonVerb(int messageLevel, const char *fmt, ...){
 	if(messageLevel <= LOGLEVEL){
 		va_list args;
 		va_start(args, fmt);
-		char msg[256];
+		char msg[MAXMESSAGELENGTH];
 		switch(messageLevel){
 #if LOGLEVEL >= TRACE
 		case TRACE:	printf("[LogIt-TRACE] "); break;
@@ -86,8 +86,48 @@ void _logItNonVerb(int messageLevel, const char *fmt, ...){
 		va_end(args);
 		fflush(stdout);
 	}
+}
 
+#if LOGGERSTYLE == TOFILE
+void _logItToFile(int messageLevel, const char * file, const char * function, const int line, const char *fmt, ...){
+	if(messageLevel <= LOGLEVEL){
+		va_list args;
+		va_start(args, fmt);
+		char msg[MAXMESSAGELENGTH];
+		switch(messageLevel){
+#if LOGLEVEL >= TRACE
+		case TRACE:	fprintf(_logFile, "[[LogIt-TRACE][%s].%s.%d] ", file, function, line); break;
+#endif
+#if LOGLEVEL >= DEBUG
+		case DEBUG:	fprintf(_logFile, "[[LogIt-DEBUG][%s].%s.%d] ", file, function, line); break;
+#endif
+#if LOGLEVEL >= INFO
+		case INFO:	fprintf(_logFile, "[[LogIt-INFO ][%s].%s.%d] ", file, function, line); break;
+#endif
+#if LOGLEVEL >= WARN
+		case WARN:	fprintf(_logFile, "[[LogIt-WARN ][%s].%s.%d] ", file, function, line); break;
+#endif
+#if LOGLEVEL >= ERR
+		case ERR:	fprintf(_logFile, "[[LogIt-ERROR][%s].%s.%d] ", file, function, line); break;
+#endif
+#if LOGLEVEL >= FATAL
+		case FATAL:	fprintf(_logFile, "[[LogIt-FATAL][%s].%s.%d] ", file, function, line); break;
+#endif
+		default:	fprintf(_logFile, "[[LogIt-UNKNW][%s].%s.%d] ", file, function, line); break;
+		}
+		vsprintf(msg, fmt, args);
+		if(msg[strlen(msg)-1] == '\n'){
+			fprintf(_logFile, "%s", msg);
+		}
+		else{
+			fprintf(_logFile, "%s\n", msg);
+		}
+
+		va_end(args);
+		fflush(stdout);
+	}
 
 }
+#endif
 
 
--- src/Logger.h
+++ src/Logger.h
@@ -11,6 +11,8 @@
 #include <stdarg.h>
 #include <String.h>
 
+#define MAXMESSAGELENGTH 512
+
 #define OFF 0
 #define FATAL 1
 #define ERR 2
@@ -22,6 +24,7 @@
 
 #define VERBOSELOGGER 1
 #define NONVERBOSELOGGER 2
+#define TOFILE 3
 
 
 #define LOGGERSTYLE VERBOSELOGGER
@@ -31,13 +34,23 @@
 
 #if LOGGERSTYLE == VERBOSELOGGER
 	#define logIt(messageLevel, fmt, ...) _logItVerb(messageLevel, __FILE__, __func__, __LINE__, fmt, ##__VA_ARGS__)
+	#define startLogger(name)
+	#define stopLogger()
+#elif LOGGERSTYLE == TOFILE
+	#define logIt(messageLevel, fmt, ...) _logItToFile(messageLevel, __FILE__, __func__, __LINE__, fmt, ##__VA_ARGS__)
+	FILE * _logFile;
+	#define startLogger(name) _logFile = fopen(name, "w")
+	#define stopLogger() fclose(_logFile)
 #else
 	#define logIt(messageLevel, fmt, ...) _logItNonVerb(messageLevel, fmt, ##__VA_ARGS__)
+	#define startLogger(name)
+	#define stopLogger()
 #endif
 
 
 void _logItNonVerb(int messageLevel, const char *fmt, ...);
 void _logItVerb(int messageLevel, const char * file, const char * function, const int line, const char *fmt, ...);
+void _logItToFile(int messageLevel, const char * file, const char * function, const int line, const char *fmt, ...);
 
 
 
